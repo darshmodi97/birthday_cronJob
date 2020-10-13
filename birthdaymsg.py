6fce195e05ecd84c6525ace1709162cde677f2a5
Darsh Modi <darsh.modi1111@gmail.com>
2020-10-13 15:05:17 +0530

    logging added

--- birthdaymsg.py
+++ birthdaymsg.py
@@ -1,10 +1,19 @@
 from datetime import datetime
 import smtplib
 from email.message import Message
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
 import mysql.connector
+import logging
+import pytz 
 
+
+IST = pytz.timezone('Asia/Kolkata') # Indian standard time
+
+Log_format = "%(levelname)s: %(asctime)s - %(message)s"
+logging.basicConfig(filename='/home/path/to/birthday.log',
+                              level=logging.DEBUG,
+                              format=Log_format,
+                              )
+logging.info("hello")
 conn = mysql.connector.connect(
   host="localhost",
   user="root",
@@ -32,16 +41,21 @@ if row :
         to = i[4] # recipients
         name_of_recepients = i[1] # name 
         subject = "Happy Birthday"
-        msg = "Hey It's Darsh, Darsh Modi wants to wish you the many many Happy Returns Of the day and Happy Birthday to you {}. Keep Smiling ..!".format(name_of_recepients)
+        msg = "Hey It's Darsh, Darsh Modi I want to wish you the many many Happy Returns Of the day and Happy Birthday to you {}. Keep Smiling ..!".format(name_of_recepients)
         body = "Subject:{}\n\n {}".format(subject,msg) #we have to add Subject part here like this ..
+
         
-        # \U0001F382
-        server = smtplib.SMTP("smtp.gmail.com",587)
-        server.ehlo()
-        # print(server.ehlo())
-        server.starttls()
-        server.login(user,password)
-        server.sendmail(user,to,body)
-        server.quit()
+        try:
+            server = smtplib.SMTP("smtp.gmail.com",587)
+            server.ehlo()
+            # print(server.ehlo())
+            server.starttls()
+            server.login(user,password)
+            server.sendmail(user,to,body)
+            logging.info("Email sent to :{} with E-mail: {} at {}".format(name_of_recepients,to,datetime.now(IST).strftime("%d,%B %Y  %X")))
+            server.quit()
+            
+        except Exception as e :
+            print("error ",e)
 else :
    pass
